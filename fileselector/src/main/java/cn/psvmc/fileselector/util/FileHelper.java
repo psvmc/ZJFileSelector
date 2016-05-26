4b992f070c7f229b00c9589b7a31db798cfd523b
psvmc <183518918@qq.com>
2016-05-26 09:40:43 +0800

    资源修改

--- fileselector/src/main/java/cn/psvmc/fileselector/util/FileHelper.java
+++ fileselector/src/main/java/cn/psvmc/fileselector/util/FileHelper.java
@@ -6,54 +6,55 @@ import cn.psvmc.fileselector.R;
 
 
 public class FileHelper {
-	
-	/**
-	 * 根据文件名后缀返回图片资源id
-	 * */
-	public static int getImageBySuffix(String suffix) {
-		int resource = R.drawable.ic_file_default;
-		if (suffix.equalsIgnoreCase("doc") || suffix.equalsIgnoreCase("docx")) {
-			resource = R.drawable.ic_file_doc;
-		} else if (suffix.equalsIgnoreCase("xls") || suffix.equalsIgnoreCase("xlsx")) {
-			resource = R.drawable.ic_file_xls;
-		} else if (suffix.equalsIgnoreCase("ppt") || suffix.equalsIgnoreCase("pptx")) {
-			resource = R.drawable.ic_file_ppt;
-		} else if (suffix.equalsIgnoreCase("png") || suffix.equalsIgnoreCase("jpg") || suffix.equalsIgnoreCase("jpeg") || suffix.equalsIgnoreCase("bmp")) {
-			resource = R.drawable.ic_file_img;
-		} else if (suffix.equalsIgnoreCase("pdf")) {
-			resource = R.drawable.ic_file_pdf;
-		} else if (suffix.equalsIgnoreCase("txt")) {
-			resource = R.drawable.ic_file_txt;
-		} else if (suffix.equalsIgnoreCase("zip") || suffix.equalsIgnoreCase("rar")) {
-			resource = R.drawable.ic_file_rar;
-		} else if (suffix.equalsIgnoreCase("dps")) {
-			resource = R.drawable.ic_file_dps;
-		} else if (suffix.equalsIgnoreCase("mp3") || suffix.equalsIgnoreCase("wav") || suffix.equalsIgnoreCase("ape") || suffix.equalsIgnoreCase("flac") || suffix.equalsIgnoreCase("wave") || suffix.equalsIgnoreCase("amr") || suffix.equalsIgnoreCase("aac") || suffix.equalsIgnoreCase("mid")) {
-			resource = R.drawable.ic_file_music;
-		} else if (suffix.equalsIgnoreCase("mp4") || suffix.equalsIgnoreCase("avi") || suffix.equalsIgnoreCase("rvmb") || suffix.equalsIgnoreCase("mkv") || suffix.equalsIgnoreCase("rm") || suffix.equalsIgnoreCase("mpg") || suffix.equalsIgnoreCase("3gp") || suffix.equalsIgnoreCase("vob") || suffix.equalsIgnoreCase("mpeg") || suffix.equalsIgnoreCase("mpg") || suffix.equalsIgnoreCase("mov") || suffix.equalsIgnoreCase("flv")) {
-			resource = R.drawable.ic_file_video;
-		} else if (suffix.equalsIgnoreCase("apk")) {
-			resource = R.drawable.ic_file_apk;
-		}
-		return resource;
-	}
-	
-	/**
-	 * 转换文件大小
-	 * */
-	public static String FormetFileSize(long size) {//转换文件大小
-		DecimalFormat df = new DecimalFormat("#.00");
-		String result = "";
-		if (size < 1024) {
-			result = df.format((double) size) + "B";
-		} else if (size < 1048576) {
-			result = df.format((double) size / 1024) + "K";
-		} else if (size < 1073741824) {
-			result = df.format((double) size / 1048576) + "M";
-		} else {
-			result = df.format((double) size / 1073741824) + "G";
-		}
-		return result;
-	}
-	
+
+    /**
+     * 根据文件名后缀返回图片资源id
+     */
+    public static int getImageBySuffix(String suffix) {
+        int resource = R.drawable.ic_file_default;
+        suffix = suffix.toLowerCase();
+        if (suffix.equals("doc") || suffix.equals("docx")) {
+            resource = R.drawable.ic_file_doc;
+        } else if (suffix.equals("xls") || suffix.equals("xlsx")) {
+            resource = R.drawable.ic_file_xls;
+        } else if (suffix.equals("ppt") || suffix.equals("pptx")) {
+            resource = R.drawable.ic_file_ppt;
+        } else if (suffix.equals("png") || suffix.equals("jpg") || suffix.equals("jpeg") || suffix.equals("bmp")) {
+            resource = R.drawable.ic_file_img;
+        } else if (suffix.equals("pdf")) {
+            resource = R.drawable.ic_file_pdf;
+        } else if (suffix.equals("txt")) {
+            resource = R.drawable.ic_file_txt;
+        } else if (suffix.equals("zip") || suffix.equals("rar")) {
+            resource = R.drawable.ic_file_rar;
+        } else if (suffix.equals("dps")) {
+            resource = R.drawable.ic_file_dps;
+        } else if (suffix.equals("mp3") || suffix.equals("wav") || suffix.equals("ape") || suffix.equals("flac") || suffix.equals("wave") || suffix.equals("amr") || suffix.equals("aac") || suffix.equals("mid")) {
+            resource = R.drawable.ic_file_music;
+        } else if (suffix.equals("mp4") || suffix.equals("avi") || suffix.equals("rvmb") || suffix.equals("mkv") || suffix.equals("rm") || suffix.equals("mpg") || suffix.equals("3gp") || suffix.equals("vob") || suffix.equals("mpeg") || suffix.equals("mpg") || suffix.equals("mov") || suffix.equals("flv")) {
+            resource = R.drawable.ic_file_video;
+        } else if (suffix.equals("apk")) {
+            resource = R.drawable.ic_file_apk;
+        }
+        return resource;
+    }
+
+    /**
+     * 转换文件大小
+     */
+    public static String FormetFileSize(long size) {//转换文件大小
+        DecimalFormat df = new DecimalFormat("#.00");
+        String result = "";
+        if (size < 1024) {
+            result = df.format((double) size) + "B";
+        } else if (size < 1048576) {
+            result = df.format((double) size / 1024) + "K";
+        } else if (size < 1073741824) {
+            result = df.format((double) size / 1048576) + "M";
+        } else {
+            result = df.format((double) size / 1073741824) + "G";
+        }
+        return result;
+    }
+
 }
